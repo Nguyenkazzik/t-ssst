73e55fde2cfe72aad5d7da8e500dd3da635a00b0
Nguyenkazzik <162619902+Nguyenkazzik@users.noreply.github.com>
2026-03-04 14:36:11 +0000

    uuuu

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ function progressInterva(){
     if(count == 100 && percentWidth == 100){
         clearInterval(progressLoad)
         percent.textContent = "Ok rùi đó:)";
+        document.getElementById("smallImg").style.opacity = "1";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
